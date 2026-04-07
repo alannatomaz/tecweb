d6ad892be04bb1b505f538ab9e58cc6b3ee398f0
alannatomaz <alannatomaz2@gmail.com>
2026-04-06 21:19:33 -0300

    feat: conclui pratica03. Fecha #3

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,22 @@
 import './style.css'
+
+const form = document.getElementById('form-login')
+const email = document.getElementById('email')
+const senha = document.getElementById('senha')
+const erroEmail = document.getElementById('erro-email')
+const erroSenha = document.getElementById('erro-senha')
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+
+  erroEmail.textContent = ''
+  erroSenha.textContent = ''
+
+  if (email.value === '') {
+    erroEmail.textContent = 'O campo de email é obrigatório.'
+  }
+
+  if (senha.value === '') {
+    erroSenha.textContent = 'O campo de senha é obrigatório.'
+  }
+})
